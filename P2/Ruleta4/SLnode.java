6c3457c4780ecc6a6f7a86271a5f5e73ace9014b
monterhumr <monterhector@gmail.com>
2017-10-09 07:10:56 -0500

    Create SLnode.java

---
+++ P2/Ruleta4/SLnode.java
@@ -0,0 +1,13 @@
+/* Node for singly linked list */
+class SLnode
+{
+    SLnode next;
+    int data;
+ 
+    /* Constructor */
+    public SLnode(int x)
+    {
+        data = x;
+        next = null;
+    }
+}
